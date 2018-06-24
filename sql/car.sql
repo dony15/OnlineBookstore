d1b38c2de6e30b3c30bfcd65208d2c796e2454cb
DonY15 <37318228+625432639@users.noreply.github.com>
2018-06-24 18:38:55 +0800

    DatabaseDesign
    1.0

---
+++ sql/car.sql
@@ -0,0 +1,2 @@
+INSERT INTO `car` (`uid`, `pid`, `count`) VALUES (0000000018, 0000000003, 1);
+INSERT INTO `car` (`uid`, `pid`, `count`) VALUES (0000000018, 0000000030, 10);
---
+++ sql/order.sql
@@ -0,0 +1,11 @@
+INSERT INTO `order` (`id`, `prices`, `receiveraddress`, `receivername`, `receiverphone`, `paystate`, `ordertime`, `uid`) VALUES ('15297567621998', 53.00, '青岛', '小哥哥', '10010', 0, '2018-06-23 20:26:02', 0000000008);
+INSERT INTO `order` (`id`, `prices`, `receiveraddress`, `receivername`, `receiverphone`, `paystate`, `ordertime`, `uid`) VALUES ('15297569428128', 65.00, '青岛', '小哥哥', '10010', 0, '2018-06-23 20:29:02', 0000000008);
+INSERT INTO `order` (`id`, `prices`, `receiveraddress`, `receivername`, `receiverphone`, `paystate`, `ordertime`, `uid`) VALUES ('15297589945738', 168.00, '青岛', '小哥哥', '10010', 0, '2018-06-23 21:03:14', 0000000008);
+INSERT INTO `order` (`id`, `prices`, `receiveraddress`, `receivername`, `receiverphone`, `paystate`, `ordertime`, `uid`) VALUES ('15297614504268', 0.00, '青岛', '小哥哥', '10010', 0, '2018-06-23 21:44:10', 0000000008);
+INSERT INTO `order` (`id`, `prices`, `receiveraddress`, `receivername`, `receiverphone`, `paystate`, `ordertime`, `uid`) VALUES ('15297614825698', 53.00, '青岛', '小哥哥', '10010', 0, '2018-06-23 21:44:42', 0000000008);
+INSERT INTO `order` (`id`, `prices`, `receiveraddress`, `receivername`, `receiverphone`, `paystate`, `ordertime`, `uid`) VALUES ('15298106107718', 0.00, '青岛', '小哥哥', '10010', 0, '2018-06-24 11:23:30', 0000000008);
+INSERT INTO `order` (`id`, `prices`, `receiveraddress`, `receivername`, `receiverphone`, `paystate`, `ordertime`, `uid`) VALUES ('15298106293718', 0.00, '青岛', '小哥哥', '10010', 0, '2018-06-24 11:23:49', 0000000008);
+INSERT INTO `order` (`id`, `prices`, `receiveraddress`, `receivername`, `receiverphone`, `paystate`, `ordertime`, `uid`) VALUES ('15298110234458', 0.00, '青岛', '小哥哥', '10010', 0, '2018-06-24 11:30:23', 0000000008);
+INSERT INTO `order` (`id`, `prices`, `receiveraddress`, `receivername`, `receiverphone`, `paystate`, `ordertime`, `uid`) VALUES ('15298112430388', 0.00, '青岛', '小哥哥', '10010', 0, '2018-06-24 11:34:03', 0000000008);
+INSERT INTO `order` (`id`, `prices`, `receiveraddress`, `receivername`, `receiverphone`, `paystate`, `ordertime`, `uid`) VALUES ('15298325717028', 65.00, '青岛', '小哥哥', '10010', 0, '2018-06-24 17:29:31', 0000000008);
+INSERT INTO `order` (`id`, `prices`, `receiveraddress`, `receivername`, `receiverphone`, `paystate`, `ordertime`, `uid`) VALUES ('15298330108468', 118.00, '青岛', '小哥哥', '10010', 0, '2018-06-24 17:36:50', 0000000008);
